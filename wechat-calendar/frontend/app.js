8a9eb0e1eb8184dcdf33c2d4dd701b0d32e32c0b
Jekye <744199255@qq.com>
2026-04-14 15:39:39 +0800

    微信云托管前端调用

--- wechat-calendar/frontend/app.js
+++ wechat-calendar/frontend/app.js
@@ -3,10 +3,21 @@ App({
   globalData: {
     openid: '',
     userInfo: null,
-    baseUrl: 'http://localhost:8080',  // 替换为你的服务器地址
+
+    // 云托管配置
+    cloudEnv: 'prod-0g6c5yfpa326bfaf',
+    serviceName: 'django-ifdx',
+
+    // 不再需要走域名/baseUrl
+    // baseUrl: 'http://localhost:8080',
   },
 
   onLaunch() {
+    // 新增：初始化云环境（只需要一次）
+    wx.cloud.init({
+      env: this.globalData.cloudEnv
+    })
+
     const openid = wx.getStorageSync('openid')
     const userInfo = wx.getStorageSync('userInfo')
     if (openid) {
@@ -18,13 +29,23 @@ App({
   request(options) {
     const openid = this.globalData.openid
     return new Promise((resolve, reject) => {
-      wx.request({
-        url: this.globalData.baseUrl + options.url,
+      wx.cloud.callContainer({
+        config: { env: this.globalData.cloudEnv },
+
+        // 直接用 options.url
+        path: options.url,
+
         method: options.method || 'GET',
         data: options.data,
         header: {
           'Content-Type': 'application/json',
+
+          // 云托管必须带
+          'X-WX-SERVICE': this.globalData.serviceName,
+
+          // 兼容你当前后端鉴权（依赖 Header x_user_openid）
           'x-user-openid': openid,
+
           ...options.header,
         },
         success(res) {
