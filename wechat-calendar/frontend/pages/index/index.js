c79762e6c216df8b2409a439cb643b633e4146bb
Jekye <744199255@qq.com>
2026-04-14 16:30:33 +0800

    change frontend login

--- wechat-calendar/frontend/pages/index/index.js
+++ wechat-calendar/frontend/pages/index/index.js
@@ -24,57 +24,57 @@ Page({
     if (app.globalData.openid) this.loadCalendars()
   },
 
-//   doLogin() {
-//     wx.login({
-//       success: (res) => {
-//         wx.getUserProfile({
-//           desc: '用于完善用户信息',
-//           success: (profileRes) => {
-//             app.request({
-//               url: '/auth/login',
-//               method: 'POST',
-//               data: {
-//                 code: res.code,
-//                 nick_name: profileRes.userInfo.nickName,
-//                 avatar_url: profileRes.userInfo.avatarUrl,
-//               }
-//             }).then(data => {
-//               app.globalData.openid = data.openid
-//               app.globalData.userInfo = data.user
-//               wx.setStorageSync('openid', data.openid)
-//               wx.setStorageSync('userInfo', data.user)
-//               this.setData({ userInfo: data.user })
-//               this.loadCalendars()
-//             })
-//           },
-//           fail: () => {
-//             wx.showToast({ title: '需要授权才能使用', icon: 'none' })
-//           }
-//         })
-//       }
-//     })
-//   },
-
-doLogin() {
-    // 开发模式：直接用固定 openid 登录，无需真实微信授权
-    const mockCode = 'user_001'  // 改成不同字符串可模拟不同用户
-    app.request({
-      url: '/auth/login',
-      method: 'POST',
-      data: {
-        code: mockCode,
-        nick_name: '测试用户',
-        avatar_url: '',
+  doLogin() {
+    wx.login({
+      success: (res) => {
+        wx.getUserProfile({
+          desc: '用于完善用户信息',
+          success: (profileRes) => {
+            app.request({
+              url: '/auth/login',
+              method: 'POST',
+              data: {
+                code: res.code,
+                nick_name: profileRes.userInfo.nickName,
+                avatar_url: profileRes.userInfo.avatarUrl,
+              }
+            }).then(data => {
+              app.globalData.openid = data.openid
+              app.globalData.userInfo = data.user
+              wx.setStorageSync('openid', data.openid)
+              wx.setStorageSync('userInfo', data.user)
+              this.setData({ userInfo: data.user })
+              this.loadCalendars()
+            })
+          },
+          fail: () => {
+            wx.showToast({ title: '需要授权才能使用', icon: 'none' })
+          }
+        })
       }
-    }).then(data => {
-      app.globalData.openid = data.openid
-      app.globalData.userInfo = data.user
-      wx.setStorageSync('openid', data.openid)
-      wx.setStorageSync('userInfo', data.user)
-      this.setData({ userInfo: data.user })
-      this.loadCalendars()
     })
   },
+
+// doLogin() {
+//     // 开发模式：直接用固定 openid 登录，无需真实微信授权
+//     const mockCode = 'user_001'  // 改成不同字符串可模拟不同用户
+//     app.request({
+//       url: '/auth/login',
+//       method: 'POST',
+//       data: {
+//         code: mockCode,
+//         nick_name: '测试用户',
+//         avatar_url: '',
+//       }
+//     }).then(data => {
+//       app.globalData.openid = data.openid
+//       app.globalData.userInfo = data.user
+//       wx.setStorageSync('openid', data.openid)
+//       wx.setStorageSync('userInfo', data.user)
+//       this.setData({ userInfo: data.user })
+//       this.loadCalendars()
+//     })
+//   },
   loadCalendars() {
     this.setData({ loading: true })
     app.request({ url: '/calendars' }).then(data => {
